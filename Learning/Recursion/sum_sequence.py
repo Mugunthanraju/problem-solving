30b0cf6cb81c5546e83710b7f7d0a4e6032eed2d
G Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2020-10-13 13:50:37 +0530

    Programmed sequence of sum

---
+++ Learning/Recursion/sum_sequence.py
@@ -0,0 +1,11 @@
+def linear_sum(S, n):
+    '''Return the sum of the first n numbers of sequence S.'''
+    if n == 0:
+        return 0
+    else:
+        return linear_sum(S, n - 1) + S[n-1]
+
+l = [1,4,2,6,3,5,9,7,10,8]
+k = len(l)
+
+print(linear_sum(l, k))
