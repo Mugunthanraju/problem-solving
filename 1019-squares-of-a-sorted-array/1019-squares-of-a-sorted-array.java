6fe3d280c11ee0f846eedc5770a5323ccda9cbac
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2025-06-22 18:00:42 +0530

    Time: 1 ms (100%), Space: 47.2 MB (54.22%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n]; // Storing square numbers
+        int left = 0, right = n - 1;
+        
+        // Reuse n in result indexing
+        for (n = n - 1; n > -1; n--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                // Find greater absolute value & store the square of it
+                result[n] = nums[left] * nums[left];
+                left++;
+            } else {
+                // Store lesser or equal absolute sqaure value
+                result[n] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
