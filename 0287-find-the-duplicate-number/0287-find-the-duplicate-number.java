81684956bb7d80807e5ff8d6fb787182e1fc1d69
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2025-06-25 15:29:50 +0530

    Time: 4 ms (89.03%), Space: 58.1 MB (66.32%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,16 +1,19 @@
 class Solution {
     public int findDuplicate(int[] nums) {
 
-        // Need to revisit to understand the concept clearly
-
         if (nums.length > 1) {
+
+            // Step 1: Use Floyd's Tortoise and Hare to find the intersection point
+
             int slow = nums[0];
             int fast = nums[nums[0]];
 
             while (slow != fast) {
                 slow = nums[slow];
                 fast = nums[nums[fast]];
-            }
+            } // Step 1 will just proves a cycle exists
+
+            // Step 2: Find the entrance to the cycle (duplicate number)
 
             fast = 0;
 
@@ -19,7 +22,7 @@ class Solution {
                 fast = nums[fast];
             }
 
-            return slow;
+            return slow; // Step 2: Actual duplicate number
         }
 
         return -1;
