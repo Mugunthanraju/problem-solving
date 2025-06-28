f19789db28e8cef8f23a89fca388c06e8a6c8bbf
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2025-06-28 14:03:16 +0530

    Time: 1 ms (60.09%), Space: 44.5 MB (95.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int count = 1;
 
         for (int i = 1; i < nums.length; i++) {
-            if (nums[count - 1] != nums[i]) // Check previous numbers is equal to jth index item.
+            if (nums[count - 1] != nums[i]) // Check previous numbers is equal to ith index item.
                 nums[count++] = nums[i]; // If not, copy the value in count index and increment count.
         }
 
