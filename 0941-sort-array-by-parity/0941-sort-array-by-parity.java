c4378a5bfca179e804badb62bb94384da278a613
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2025-07-06 00:11:42 +0530

    Time: 0 ms (100%), Space: 44.5 MB (99.06%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -5,38 +5,38 @@ class Solution {
 
         /* Solution - 1 : In place Array Modification */ 
 
-        int temp;
-        while (left < right) {
-            if (nums[left] % 2 != 0 && nums[right] % 2 == 0) {
-                /* Odd @ L & Even @ R then swap */
-                temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
-            }
-            else if (nums[left] % 2 == 0) {
-                /* Even @ L */
-                left++;
-            }
-            else {
-                /* Odd @ R */
-                right--;
-            }
-        }
-        return nums;
+        // int temp;
+        // while (left < right) {
+        //     if (nums[left] % 2 != 0 && nums[right] % 2 == 0) {
+        //         /* Odd @ L & Even @ R then swap */
+        //         temp = nums[left];
+        //         nums[left] = nums[right];
+        //         nums[right] = temp;
+        //         left++;
+        //         right--;
+        //     }
+        //     else if (nums[left] % 2 == 0) {
+        //         /* Even @ L */
+        //         left++;
+        //     }
+        //     else {
+        //         /* Odd @ R */
+        //         right--;
+        //     }
+        // }
+        // return nums;
 
         /* Solution - 2 : Create new array with same length for result */
 
-        // int[] result = new int[nums.length];
-        // for (int num : nums) {
-        //     if (num % 2 == 0) {
-        //         result[left++] = num;
-        //     } else {
-        //         result[right--] = num;
-        //     }
-        // }
-        // return result;
+        int[] result = new int[nums.length];
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                result[left++] = num;
+            } else {
+                result[right--] = num;
+            }
+        }
+        return result;
 
     }
 }
