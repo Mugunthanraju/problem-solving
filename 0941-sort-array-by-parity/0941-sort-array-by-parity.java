cc8d99eacba31f2b8f0daaf2a95a1564b620a5a0
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2025-07-06 00:11:22 +0530

    Time: 1 ms (51.21%), Space: 44.8 MB (77.83%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,9 +1,14 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int left = 0 , right = nums.length - 1, temp;
 
+        int left = 0 , right = nums.length - 1;
+
+        /* Solution - 1 : In place Array Modification */ 
+
+        int temp;
         while (left < right) {
             if (nums[left] % 2 != 0 && nums[right] % 2 == 0) {
+                /* Odd @ L & Even @ R then swap */
                 temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
@@ -11,14 +16,27 @@ class Solution {
                 right--;
             }
             else if (nums[left] % 2 == 0) {
+                /* Even @ L */
                 left++;
             }
             else {
+                /* Odd @ R */
                 right--;
             }
         }
-
         return nums;
+
+        /* Solution - 2 : Create new array with same length for result */
+
+        // int[] result = new int[nums.length];
+        // for (int num : nums) {
+        //     if (num % 2 == 0) {
+        //         result[left++] = num;
+        //     } else {
+        //         result[right--] = num;
+        //     }
+        // }
+        // return result;
 
     }
 }
