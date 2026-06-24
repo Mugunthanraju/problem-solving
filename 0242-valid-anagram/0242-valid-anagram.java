8fbd90eb021e635da3fa1dd74d21cd91bdaf3ec5
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-06-24 16:14:06 +0530

    Time: 5 ms (61.92%), Space: 44.5 MB (66.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        if (s.length() != t.length())
+            return false; // In either approach this is reasonable
+
+
         // /* If string contains only lowercase english letters */
-        // int[] alphabet = new int[26];
-        // for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        // for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        // for (int i : alphabet) if (i != 0) return false;
+
+        int[] alphabet = new int[26]; // frequency of small letters
+        for (int i = 0; i < s.length(); i++) {
+            alphabet[s.charAt(i) - 'a']++; // increment the frequency
+            alphabet[t.charAt(i) - 'a']--; // decrement the frequency
+        }
+        for (int i : alphabet) 
+            if (i != 0) // value will be zero if both strings has same char freq
+                return false; 
 
         /* 
+            ====================================================
             Follow up : if the inputs contain Unicode characters 
         */
 
-        if (s.length() != t.length())
-            return false;
 
-        Map<Character, Integer> frequency = new HashMap<>();
+        // Map<Character, Integer> frequency = new HashMap<>();
 
-        for (int index = 0; index < s.length(); index++) {
-            frequency.put(s.charAt(index), frequency.getOrDefault(s.charAt(index), 0) + 1);
-            frequency.put(t.charAt(index), frequency.getOrDefault(t.charAt(index), 0) - 1);
-        }
+        // for (int index = 0; index < s.length(); index++) {
+        //     frequency.put(s.charAt(index), frequency.getOrDefault(s.charAt(index), 0) + 1);
+        //     frequency.put(t.charAt(index), frequency.getOrDefault(t.charAt(index), 0) - 1);
+        // }
 
-        for(char c : frequency.keySet()) {
-            if (frequency.get(c) != 0)
-                return false;
-        }
+        // for(char c : frequency.keySet()) {
+        //     if (frequency.get(c) != 0)
+        //         return false;
+        // }
 
         return true; // This is same for any case
     }
