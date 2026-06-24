82e8373edfb62ebe5f08192cd5fd503b603ad30a
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-06-24 16:34:30 +0530

    Time: 15 ms (43.85%), Space: 19.4 MB (44.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,38 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if (len(s) != len(t)):
-            return False
+            return False # In either approach this is reasonable
         
-        frequency = {}
+        """
+            string contains only lowercase english letters
+        """
+        # freq = [0] * 26 # frequency of small letters
+        # for i in range(len(s)):
+        #     # increment the frequency
+        #     freq[ord(s[i]) - ord('a')] += 1
+        #     # decrement the frequency
+        #     freq[ord(t[i]) - ord('a')] -= 1
+        
+        # for i in range(len(freq)):
+        #     # val will be zero if both str has same char freq
+        #     if freq[i] != 0:
+        #         return False
 
-        for c in s:
-            if c in frequency:
-                frequency[c] += 1
-            else:
-                frequency[c] = 1
+        """
+            ====================================================
+            Follow up : if the inputs contain Unicode characters 
+        """
         
-        for c in t:
-            if c not in frequency or frequency[c] < 1:
+        frequency = dict() # Store frequency of Unicode characters
+        for i in range(len(s)):
+            # increment the frequency
+            frequency[s[i]] = frequency.get(s[i], 0) + 1
+            # decrement the frequency
+            frequency[t[i]] = frequency.get(t[i], 0) - 1
+        
+        for val in frequency.values():
+            # val will be zero if both str has same char freq
+            if val != 0:
                 return False
-            frequency[c] -= 1
         
-        return True and (len(s) == len(t))
+        return True
