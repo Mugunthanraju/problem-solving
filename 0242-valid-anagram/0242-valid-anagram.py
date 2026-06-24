e826f6afd786934021d54d633af90ac4d4bde9ef
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-06-24 16:37:33 +0530

    Time: 15 ms (43.85%), Space: 19.3 MB (75.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,33 +6,33 @@ class Solution:
         """
             string contains only lowercase english letters
         """
-        freq = [0] * 26 # frequency of small letters
-        for i in range(len(s)):
-            # increment the frequency
-            freq[ord(s[i]) - ord('a')] += 1
-            # decrement the frequency
-            freq[ord(t[i]) - ord('a')] -= 1
+        # freq = [0] * 26 # frequency of small letters
+        # for i in range(len(s)):
+        #     # increment the frequency
+        #     freq[ord(s[i]) - ord('a')] += 1
+        #     # decrement the frequency
+        #     freq[ord(t[i]) - ord('a')] -= 1
         
-        for i in range(len(freq)):
-            # val will be zero if both str has same char freq
-            if freq[i] != 0:
-                return False
+        # for i in range(len(freq)):
+        #     # val will be zero if both str has same char freq
+        #     if freq[i] != 0:
+        #         return False
 
         """
             ====================================================
             Follow up : if the inputs contain Unicode characters 
         """
         
-        # frequency = dict() # Store frequency of Unicode characters
-        # for i in range(len(s)):
-        #     # increment the frequency
-        #     frequency[s[i]] = frequency.get(s[i], 0) + 1
-        #     # decrement the frequency
-        #     frequency[t[i]] = frequency.get(t[i], 0) - 1
+        frequency = dict() # Store frequency of Unicode characters
+        for i in range(len(s)):
+            # increment the frequency
+            frequency[s[i]] = frequency.get(s[i], 0) + 1
+            # decrement the frequency
+            frequency[t[i]] = frequency.get(t[i], 0) - 1
         
-        # for val in frequency.values():
-        #     # val will be zero if both str has same char freq
-        #     if val != 0:
-        #         return False
+        for val in frequency.values():
+            # val will be zero if both str has same char freq
+            if val != 0:
+                return False
         
         return True
