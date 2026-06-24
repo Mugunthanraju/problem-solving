4ed326ade358610d5f8c593bcdcbe4226e09aefc
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-06-24 16:18:59 +0530

    Time: 27 ms (5.42%), Space: 44.3 MB (74.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,33 +6,34 @@ class Solution {
 
 
         // /* If string contains only lowercase english letters */
-
-        int[] alphabet = new int[26]; // frequency of small letters
-        for (int i = 0; i < s.length(); i++) {
-            alphabet[s.charAt(i) - 'a']++; // increment the frequency
-            alphabet[t.charAt(i) - 'a']--; // decrement the frequency
-        }
-        for (int i : alphabet) 
-            if (i != 0) // value will be zero if both strings has same char freq
-                return false; 
+        // int[] alphabet = new int[26]; // frequency of small letters
+        // for (int i = 0; i < s.length(); i++) {
+        //     alphabet[s.charAt(i) - 'a']++; // increment the frequency
+        //     alphabet[t.charAt(i) - 'a']--; // decrement the frequency
+        // }
+        // for (int i : alphabet) 
+        //     if (i != 0) // val will be zero if both str has same char freq
+        //         return false; 
 
         /* 
             ====================================================
             Follow up : if the inputs contain Unicode characters 
         */
 
+        // Store frequency of Unicode characters
+        Map<Character, Integer> frequency = new HashMap<>();
+        for (int index = 0; index < s.length(); index++) {
+            // increment the frequency
+            frequency.put(s.charAt(index), frequency.getOrDefault(s.charAt(index), 0) + 1);
+            // decrement the frequency
+            frequency.put(t.charAt(index), frequency.getOrDefault(t.charAt(index), 0) - 1);
+        }
 
-        // Map<Character, Integer> frequency = new HashMap<>();
-
-        // for (int index = 0; index < s.length(); index++) {
-        //     frequency.put(s.charAt(index), frequency.getOrDefault(s.charAt(index), 0) + 1);
-        //     frequency.put(t.charAt(index), frequency.getOrDefault(t.charAt(index), 0) - 1);
-        // }
-
-        // for(char c : frequency.keySet()) {
-        //     if (frequency.get(c) != 0)
-        //         return false;
-        // }
+        for(char c : frequency.keySet()) {
+            // val will be zero if both str has same char freq
+            if (frequency.get(c) != 0)
+                return false;
+        }
 
         return true; // This is same for any case
     }
