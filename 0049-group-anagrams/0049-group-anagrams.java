01e96c92e69c13a57f7b1f04bc0195b15f5e736d
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-06-28 10:12:06 +0530

    Time: 18 ms (22.02%), Space: 50.2 MB (18.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -4,10 +4,27 @@ class Solution {
 
         for (String word : strs) {
 
-          // sorting the chars in str and then convert char array into str
-          char[] chars = word.toCharArray();
-          Arrays.sort(chars);
-          String keyword = new String(chars);
+          // /* 
+          //   sorting the chars in str and then convert char array into str
+          // */
+          // char[] chars = word.toCharArray();
+          // Arrays.sort(chars);
+          // String keyword = new String(chars);
+
+          int[] count = new int[26];
+            
+            // Count character frequencies
+            for (char c : word.toCharArray()) {
+                count[c - 'a']++;
+            }
+            
+            // Build a string signature from the frequency array
+            StringBuilder sb = new StringBuilder();
+            for (int val : count) {
+                sb.append('#');
+                sb.append(val);
+            }
+            String keyword = sb.toString();
 
           // Make sure there is arraylist for every keyword to add word
           groups.putIfAbsent(keyword, new ArrayList<>());
