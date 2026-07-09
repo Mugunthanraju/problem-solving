b1b79bf1c17a0b258ac9eea59764df19a14d05b3
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-07-09 23:23:03 +0530

    Time: 0 ms (100%), Space: 43.8 MB (8.85%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,15 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        int left = 0, right = nums.length;
 
-        int index = 0; // Used to in place items except val
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                // Omitting the val from array with the help of condition
-                nums[index++] = nums[i]; 
-            }
+        while (left < right) {
+          if (nums[left] == val) {
+            nums[left] = nums[right - 1];
+            --right;
+          } else {
+            ++left;
+          }
         }
 
-        return index; // Returns number of item count without val
+        return right;
     }
 }
