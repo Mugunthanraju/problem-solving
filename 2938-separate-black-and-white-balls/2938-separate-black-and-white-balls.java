e8c777678a367da1589c1a90fc9c9f1f4fd84528
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-07-11 23:12:24 +0530

    Time: 8 ms (51.53%), Space: 47.6 MB (55.2%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -3,11 +3,12 @@ class Solution {
       if (s.length() < 2)
         return 0;
 
-      long swap = 0, black = 0;
+      long swap = 0;
+      int black = 0;
 
       for (int i = 0; i < s.length(); i++) {
         if (s.charAt(i) == '0')
-          swap += black;
+          swap += (long) black;
         else
           ++black;
       }
