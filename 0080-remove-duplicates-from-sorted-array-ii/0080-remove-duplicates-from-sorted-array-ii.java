090cd14d558a638e009d1c594f685e7797099af6
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-08-02 18:45:41 +0530

    Time: 0 ms (100%), Space: 48.7 MB (70.47%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if (nums.length < 3)
+          return nums.length;
+        
+        int k = 2, i;
 
-        // index points to the position to insert the next valid element
-        int index = 2;
-
-        for (int i = 2; i < nums.length; i++) {
-            // Check if the current element is not equal to the element at index - 2
-            // This ensures we allow at most two duplicates
-            if (nums[index - 2] != nums[i])
-                nums[index++] = nums[i]; 
+        for (i = 2; i < nums.length; i++) {
+          // System.out.println("Before if condition k=" + k + " i=" + i + " array=" + Arrays.toString(nums));
+          if (nums[k - 2] != nums[i]) {
+            nums[k++] = nums[i];
+          }
+          // System.out.println("After if condition k=" + k + " i=" + i + " array=" + Arrays.toString(nums));
         }
-
-        // index is the new length of the array with duplicates allowed at most twice
-        return index;
+        return k;
     }
 }
