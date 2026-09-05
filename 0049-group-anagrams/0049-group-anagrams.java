cc27e3850194b20632563505e6dfcc735462c9f9
Mugunthan Raju <54927781+Mugunthanraju@users.noreply.github.com>
2026-09-05 22:37:26 +0530

    Time: 7 ms (72.51%), Space: 49.9 MB (25.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,35 +2,13 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> groups = new HashMap<>();
 
-        for (String word : strs) {
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars); // <- TC : O( k log k)
 
-          // /* 
-          //   sorting the chars in str and then convert char array into str
-          // */
-          // char[] chars = word.toCharArray();
-          // Arrays.sort(chars);
-          // String keyword = new String(chars);
-
-          int[] count = new int[26];
-            
-            
-            /*
-              - Count character frequencies
-              - Build a string signature from the frequency array
-            */
-            for (char c : word.toCharArray()) {
-                count[c - 'a']++;
-            }
-            StringBuilder sb = new StringBuilder();
-            for (int val : count) {
-                sb.append('#');
-                sb.append(val);
-            }
-            String keyword = sb.toString();
-
-          // Make sure there is arraylist for every keyword to add word
-          groups.putIfAbsent(keyword, new ArrayList<>());
-          groups.get(keyword).add(word);
+            String key = new String(chars);
+            // Make sure there is arraylist for every keyword to add word
+            groups.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
 
         return new ArrayList<>(groups.values());
